4b8bc2a05bc3eb01b2c688aaa7ae043d41e517da
jonathan heredia <shakatoti1618@gmail.com>
2026-06-15 16:07:45 -0500

    feat: agregar documentacion OpenAPI con Swagger UI

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi.yaml'));
+
 
 app.use(express.json());
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 const authorsRouter = require('./routes/authors.routes');
 const postsRouter = require('./routes/posts.routes');
 
 app.use('/authors', authorsRouter);
 app.use('/posts', postsRouter);
+
 
 const errorHandler = require('./middlewares/errorHandler');
 app.use(errorHandler);
