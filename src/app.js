d6c9f1c5b6675009e32907c252cc78633af10a56
jonathan heredia <shakatoti1618@gmail.com>
2026-06-16 17:17:40 -0500

    fix: swagger con URL dinamica segun el entorno

--- src/app.js
+++ src/app.js
@@ -5,23 +5,32 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
 
-
 const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi.yaml'));
-
 
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+// Swagger con URL dinámica según el entorno
+app.use('/api-docs', swaggerUi.serve, (req, res, next) => {
+    const host = req.get('host');
+    const protocol = req.protocol;
+    const dynamicDoc = {
+    ...swaggerDocument,
+    servers: [
+    {
+        url: `${protocol}://${host}`,
+        description: 'Servidor actual'
+    }
+    ]
+    };
+    swaggerUi.setup(dynamicDoc)(req, res, next);
+});
 
 const authorsRouter = require('./routes/authors.routes');
 const postsRouter = require('./routes/posts.routes');
 
 app.use('/authors', authorsRouter);
 app.use('/posts', postsRouter);
-
 
 const errorHandler = require('./middlewares/errorHandler');
 app.use(errorHandler);
